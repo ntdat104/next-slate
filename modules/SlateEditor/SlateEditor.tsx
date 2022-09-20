98ce2bef713046b5943aeca65f3e7b9dc47edd9e
ntdat104 <ntd.k108cva@gmail.com>
2022-09-20 17:26:49 +0700

    update

--- modules/SlateEditor/SlateEditor.tsx
+++ modules/SlateEditor/SlateEditor.tsx
@@ -77,42 +77,13 @@ const TEXT_ALIGN_TYPES: CustomElementType[] = [
   "justify",
 ];
 
-const SlateEditor: React.FC = () => {
-  const [value, setValue] = React.useState<Descendant[]>([
-    {
-      type: "paragraph",
-      children: [
-        { text: "This is editable " },
-        { text: "rich", bold: true },
-        { text: " text, " },
-        { text: "much", italic: true },
-        { text: " better than a " },
-        { text: "<textarea>", code: true },
-        { text: "!" },
-      ],
-    },
-    {
-      type: "paragraph",
-      children: [
-        {
-          text: "Since it's rich text, you can do things like turn a selection of text ",
-        },
-        { text: "bold", bold: true },
-        {
-          text: ", or add a semantically rendered block quote in the middle of the page, like this:",
-        },
-      ],
-    },
-    {
-      type: "block-quote",
-      children: [{ text: "A wise quote." }],
-    },
-    {
-      type: "paragraph",
-      align: "center",
-      children: [{ text: "Try it out for yourself!" }],
-    },
-  ]);
+interface Props {
+  value: Descendant[];
+  setValue: (value: Descendant[]) => void;
+}
+
+const SlateEditor: React.FC<Props> = (props) => {
+  const { value, setValue } = props;
 
   const renderElement = React.useCallback(
     (props: RenderElementProps) => <Element {...props} />,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,59 @@
 import type { NextPage } from "next";
+import React from "react";
+import { Descendant } from "slate";
 import SlateEditor from "../modules/SlateEditor";
+import dynamic from "next/dynamic";
+
+const ReactJson = dynamic(() => import("react-json-view"), {
+  loading: () => <p>Loading ...</p>,
+  ssr: false,
+});
 
 const Home: NextPage = () => {
+  const [value, setValue] = React.useState<Descendant[]>([
+    {
+      type: "paragraph",
+      children: [
+        { text: "This is editable " },
+        { text: "rich", bold: true },
+        { text: " text, " },
+        { text: "much", italic: true },
+        { text: " better than a " },
+        { text: "<textarea>", code: true },
+        { text: "!" },
+      ],
+    },
+    {
+      type: "paragraph",
+      children: [
+        {
+          text: "Since it's rich text, you can do things like turn a selection of text ",
+        },
+        { text: "bold", bold: true },
+        {
+          text: ", or add a semantically rendered block quote in the middle of the page, like this:",
+        },
+      ],
+    },
+    {
+      type: "block-quote",
+      children: [{ text: "A wise quote." }],
+    },
+    {
+      type: "paragraph",
+      align: "center",
+      children: [{ text: "Try it out for yourself!" }],
+    },
+  ]);
+
   return (
-    <div style={{ maxWidth: 800, margin: 10 }}>
-      <SlateEditor />
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ width: "calc(50% - 10px)" }}>
+        <SlateEditor value={value} setValue={setValue} />
+      </div>
+      <div style={{ width: "calc(50% - 10px)" }}>
+        <ReactJson src={value} />
+      </div>
     </div>
   );
 };
